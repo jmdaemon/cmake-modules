ab13f32ae046740e13fbb5e84489cb3523ec88ce
Joseph Diza <josephm.diza@gmail.com>
2022-04-06 00:12:36 -0700

    Rename lslib to lstarget

--- Utilities.cmake
+++ Utilities.cmake
@@ -1,13 +1,13 @@
-function(lslib)
+function(lstarget)
     # Set make_library arguments
-    set(ARG_PREFIX LIB)
+    set(ARG_PREFIX TARGET)
     set(_OPTIONS_ARGS )
     set(_ONE_VALUE_ARGS NAME)
     set(_MULTI_VALUE_ARGS HEADERS)
 
     cmake_parse_arguments(${ARG_PREFIX} "${_OPTIONS_ARGS}" "${_ONE_VALUE_ARGS}" "${_MULTI_VALUE_ARGS}" ${ARGN})
 
-    message(STATUS, "Library Target Name: ${LIB_NAME}")
-    message(STATUS, "Library Headers Directory: ${LIB_HEADERS}")
+    message(STATUS, "Target Name: ${TARGET_NAME}")
+    message(STATUS, "Headers Directory: ${TARGET_HEADERS}")
 endfunction()
 
