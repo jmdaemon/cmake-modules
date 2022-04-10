9c18da913234466cc19efce337a01c22afe09e62
Joseph Diza <josephm.diza@gmail.com>
2022-04-10 12:26:43 -0700

    Create import_external_library function

--- utility.cmake
+++ utility.cmake
@@ -1,19 +1,34 @@
+# Imports a shared or static library into a CMake Project
+function(import_external_library)
+    # Set make_library arguments
+    set(ARG_PREFIX LIB)
+    set(_OPTIONS_ARGS )
+    set(_ONE_VALUE_ARGS NAME TYPE)
+    set(_MULTI_VALUE_ARGS HEADERS SOURCE_DIR PATH FILE)
+    cmake_parse_arguments(${ARG_PREFIX} "${_OPTIONS_ARGS}" "${_ONE_VALUE_ARGS}" "${_MULTI_VALUE_ARGS}" ${ARGN})
+    
+    add_library(${LIB_NAME} ${LIB_TYPE} IMPORTED)
+    # Import the library file
+    set_property(
+        TARGET ${LIB_NAME}
+        PROPERTY IMPORTED_LOCATION ${LIB_PATH})
+    # Import the library headers
+    set_property(
+        TARGET ${LIB_NAME}
+        PROPERTY INTERFACE_INCLUDE_DIRECTORIES ${LIB_HEADERS})
+    # Ensure the library exists and is not null
+    if(NOT ${LIB_NAME})
+        message(FATAL_ERROR "${LIB_NAME} not found!")
+    endif()
+endfunction()
+
 # Utility C Library
 set(UTILITY utility)
-set(UTILITY_SRC ${PROJECT_SOURCE_DIR}/subprojects/utility)
-set(UTILITY_HEADERS ${UTILITY_SRC}/include)
-set(UTILITY_LIB_PATH ${UTILITY_SRC}/build/release/lib)
-set(LIB_UTILITY ${UTILITY_LIB_PATH}/lib${UTILITY}.so)
-
-add_library(${UTILITY} SHARED IMPORTED)
-set_property(
-TARGET ${UTILITY}
-PROPERTY IMPORTED_LOCATION ${LIB_UTILITY})
-
-set_property(
-TARGET ${UTILITY}
-PROPERTY INTERFACE_INCLUDE_DIRECTORIES ${UTILITY_HEADERS})
-
-if(NOT ${UTILITY})
-    message(FATAL_ERROR "${UTILITY} not found!")
-endif()
+set(UTILITY_SRC ${PROJECT_SOURCE_DIR}/subprojects/${UTILITY})
+import_external_library(
+    NAME ${UTILITY}
+    TYPE SHARED
+    SOURCE_DIR ${UTILITY_SRC}
+    HEADERS ${UTILITY_SRC}/include
+    PATH ${UTILITY_SRC}/build/release/lib/lib${UTILITY}.so
+    )
