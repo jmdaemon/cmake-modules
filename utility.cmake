1e7e28d22bdea73d770fef789eabab04940d75ad
Joseph Diza <josephm.diza@gmail.com>
2022-04-10 12:15:25 -0700

    Reuse defined vars for utility, add null lib check

--- utility.cmake
+++ utility.cmake
@@ -1,16 +1,39 @@
 # Utility C Library
-set(UTILITY_LIB utility)
 
-set(UTILITY_LIBR    ${PROJECT_SOURCE_DIR}/subprojects/utility/build/release/lib)
-set(UTILITY_LIB_BIN ${UTILITY_LIBR}/${UTILITY_LIB}.so)
-set(UTILITY_LIBD    ${PROJECT_SOURCE_DIR}/subprojects/utility)
+set(UTILITY utility)
+set(UTILITY_SRC ${PROJECT_SOURCE_DIR}/subprojects/utility)
+set(UTILITY_HEADERS ${UTILITY_SRC}/include)
+set(UTILITY_LIB_PATH ${UTILITY_SRC}/build/release/lib)
+set(LIB_UTILITY ${UTILITY_LIB_PATH}/lib${UTILITY}.so)
+
 #set(UTILITY_SRCS    ${UTILITY_LIBD}/src/file.c)
-set(UTILITY_HEADERS ${PROJECT_SOURCE_DIR}/subprojects/utility/include)
 
-find_library(${UTILITY_LIB}
-    NAMES ${UTILITY_LIB} libutility
-    HINTS ${UTILITY_LIBP} ${UTILITY_LIB_BIN}
-    #HINTS ${UTILITY_LIBP} ${UTILITY_LIBP}/lib${UTILITY_LIB}.so ${UTILITY_LIBD}
-    #IMPORTED_LOCATION /usr/local/lib/libhello-user.dll
-    #IMPORTED_LOCATION /usr/local/lib/libhello-user.dll
-    )
+#find_library(${UTILITY_LIB}
+    #NAMES ${UTILITY_LIB} libutility
+    #HINTS ${UTILITY_LIBP} ${UTILITY_LIB_BIN}
+    ##HINTS ${UTILITY_LIBP} ${UTILITY_LIBP}/lib${UTILITY_LIB}.so ${UTILITY_LIBD}
+    ##IMPORTED_LOCATION /usr/local/lib/libhello-user.dll
+    ##IMPORTED_LOCATION /usr/local/lib/libhello-user.dll
+    #)
+
+#add_library(${UTILITY_LIB} SHARED IMPORTED)
+#set_property(
+#TARGET ${UTILITY_LIB}
+#PROPERTY IMPORTED_LOCATION ${UTILITY_LIB_BIN})
+
+#set_property(
+#TARGET ${UTILITY_LIB}
+#PROPERTY INTERFACE_INCLUDE_DIRECTORIES ${UTILITY_HEADERS})
+
+add_library(${UTILITY} SHARED IMPORTED)
+set_property(
+TARGET ${UTILITY}
+PROPERTY IMPORTED_LOCATION ${LIB_UTILITY})
+
+set_property(
+TARGET ${UTILITY}
+PROPERTY INTERFACE_INCLUDE_DIRECTORIES ${UTILITY_HEADERS})
+
+if(NOT ${UTILITY})
+    message(FATAL_ERROR "${UTILITY} not found!")
+endif()
