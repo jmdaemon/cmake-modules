70e9052a419d3b786561a5646504bab56f017deb
Joseph Diza <josephm.diza@gmail.com>
2022-04-10 17:45:40 -0700

    Add version tag to fmt

--- fmt.cmake
+++ fmt.cmake
@@ -1,4 +1,6 @@
 FetchContent_Declare(libfmt
-    GIT_REPOSITORY "https://github.com/fmtlib/fmt")
+    GIT_REPOSITORY "https://github.com/fmtlib/fmt"
+    GIT_TAG 8.1.1
+    )
 FetchContent_MakeAvailable(libfmt)
 
