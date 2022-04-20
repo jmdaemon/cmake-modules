893c7b0249498444f83b87d583b031cd37ce663e
Joseph Diza <josephm.diza@gmail.com>
2022-04-20 11:43:05 -0700

    Add initial cnc.cmake module

---
+++ cnc.cmake
@@ -0,0 +1,14 @@
+
+# If the library is installed on our system
+find_library(cnc HINTS /usr/local/lib)
+
+# If the library is not installed on our system
+if (NOT cnc) 
+    # Fetch and build it with FetchContent 
+    include(FetchContent)
+    FetchContent_Declare(
+        cnc
+        GIT_REPOSITORY https://github.com/jmdaemon/cnc.git
+        GIT_TAG master)
+    FetchContent_MakeAvailable(cnc)
+endif()
