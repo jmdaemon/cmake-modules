e125484d1a0e0f7c6876f3317125330a21732028
Joseph Diza <josephm.diza@gmail.com>
2022-04-20 12:38:46 -0700

    Add option to configure utility as a subproject

--- utility.cmake
+++ utility.cmake
@@ -1,11 +1,26 @@
+# Utility C Library
+
 # Require import_external_library cmake module function
 include(Library)
+
+set(ConfigureAsSubproject FALSE)
 
 # If the library is installed on our system
 find_library(utility HINTS /usr/local/lib)
 
 # If the library is not installed on our system
 if (NOT utility_FOUND)
+    # Configure the library as a subproject in our main repository
+    if (ConfigureAsSubproject)
+        set(UTILITY utility)
+        set(UTILITY_SRC ${PROJECT_SOURCE_DIR}/subprojects/${UTILITY})
+        import_external_library(
+            NAME ${UTILITY}
+            TYPE SHARED
+            SOURCE_DIR ${UTILITY_SRC}
+            HEADERS ${UTILITY_SRC}/include
+            PATH ${UTILITY_SRC}/build/release/lib/lib${UTILITY}.so)
+    else()
     # Fetch and build it with FetchContent 
     include(FetchContent)
     FetchContent_Declare(
@@ -13,15 +28,5 @@ if (NOT utility_FOUND)
         GIT_REPOSITORY https://github.com/jmdaemon/utility.git
         GIT_TAG master)
     FetchContent_MakeAvailable(utility)
+    endif()
 endif()
-
-# Utility C Library
-#set(UTILITY utility)
-#set(UTILITY_SRC ${PROJECT_SOURCE_DIR}/subprojects/${UTILITY})
-#import_external_library(
-    #NAME ${UTILITY}
-    #TYPE SHARED
-    #SOURCE_DIR ${UTILITY_SRC}
-    #HEADERS ${UTILITY_SRC}/include
-    #PATH ${UTILITY_SRC}/build/release/lib/lib${UTILITY}.so
-    #)
