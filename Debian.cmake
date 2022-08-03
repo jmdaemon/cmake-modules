5c42b85b38ab541054a7dcd7470d412f9aa0d388
Joseph Diza <josephm.diza@gmail.com>
2022-08-02 21:15:41 -0700

    Store debian packages in PROJECT_ROOT/_packages
    It's useful to store the old packages in a directory outside of build in
    order to keep them around for later.

--- Debian.cmake
+++ Debian.cmake
@@ -1,6 +1,59 @@
+set(CPACK_PACKAGE_NAME ${PROJECT_NAME}
+    CACHE STRING "The resulting package name")
+
+set(CPACK_PACKAGE_DESCRIPTION_SUMMARY ${CMAKE_PROJECT_DESCRIPTION}
+    CACHE STRING "Package description for the package metadata")
+
+set(CPACK_VERBATIM_VARIABLES YES)
+set(CPACK_PACKAGE_INSTALL_DIRECTORY ${CPACK_PACKAGE_NAME})
+
+# Stores the output into _packages
+# This is good for keeping an archive of all past project versions
+#set(CPACK_OUTPUT_FILE_PREFIX "${CMAKE_SOURCE_DIR}/_packages")
+#set(CPACK_PACKAGE_DIRECTORY "packages/${CMAKE_PROJECT_NAME}-${CMAKE_PROJECT_VERSION}")
+#set(CPACK_PACKAGE_DIRECTORY "packages/${CMAKE_PROJECT_NAME}-${CMAKE_PROJECT_VERSION}")
+set(CPACK_PACKAGE_DIRECTORY "${CMAKE_SOURCE_DIR}/_packages/${CMAKE_PROJECT_NAME}-${CMAKE_PROJECT_VERSION}")
+
+# Set package install path
+set(CPACK_PACKAGING_INSTALL_PREFIX "/usr/")
+
+# Set package versions
+set(CPACK_PACKAGE_VERSION_MAJOR ${PROJECT_VERSION_MAJOR})
+set(CPACK_PACKAGE_VERSION_MINOR ${PROJECT_VERSION_MINOR})
+set(CPACK_PACKAGE_VERSION_PATCH ${PROJECT_VERSION_PATCH})
+
+# Set maintainer name and email
 set(CPACK_DEBIAN_PACKAGE_MAINTAINER "$ENV{MAINTAINER_NAME}")
-set(CPACK_PACKAGE_DIRECTORY "packages/${CMAKE_PROJECT_NAME}-${CMAKE_PROJECT_VERSION}")
-set(CPACK_DEBIAN_PACKAGE_DEPENDS ${DEB_DEPENDS})
+set(CPACK_PACKAGE_CONTACT "$ENV{MAINTAINER_EMAIL}")
+
+# Include project resource files
+set(CPACK_RESOURCE_FILE_LICENSE "${CMAKE_CURRENT_SOURCE_DIR}/LICENSE")
+set(CPACK_RESOURCE_FILE_README "${CMAKE_CURRENT_SOURCE_DIR}/README.md")
+
 set(CPACK_PACKAGE_DESCRIPTION_FILE "${CMAKE_CURRENT_SOURCE_DIR}/README.md")
-set(CPACK_GENERATOR "DEB;TGZ;ZIP") # Create .deb, .tgz, .zip files
+
+# Naming scheme for Debian packages
+# When set, this generates app_0.1.0_amd64.deb
+# instead of app-0.1.0-Linux.deb (note the underscores)
+#set(CPACK_DEBIAN_FILE_NAME DEB-DEFAULT)
+set(CPACK_DEBIAN_FILE_NAME DEB-DEFAULT)
+#set(${CPACK_PACKAGE_NAME}-${CPACK_PACKAGE_VERSION}-${CPACK_SYSTEM_NAME})
+
+# Create .deb, .tgz, .zip files
+set(CPACK_GENERATOR "DEB;TGZ;ZIP")
+
+# if you want every group to have its own package,
+# although the same happens if this is not sent (so it defaults to ONE_PER_GROUP)
+# and CPACK_DEB_COMPONENT_INSTALL is set to YES
+#set(CPACK_COMPONENTS_GROUPING ALL_COMPONENTS_IN_ONE)#ONE_PER_GROUP)
+# without this you won't be able to pack only specified component
+#set(CPACK_DEB_COMPONENT_INSTALL YES)
+
+set(CPACK_DEBIAN_PACKAGE_DEPENDS ${DEB_DEPENDS})
+
 include(CPack)
+
+#set(CPACK_DEBIAN_PACKAGE_MAINTAINER "$ENV{MAINTAINER_NAME}")
+#set(CPACK_PACKAGE_DIRECTORY "packages/${CMAKE_PROJECT_NAME}-${CMAKE_PROJECT_VERSION}")
+#set(CPACK_PACKAGE_DESCRIPTION_FILE "${CMAKE_CURRENT_SOURCE_DIR}/README.md")
+#include(CPack)
