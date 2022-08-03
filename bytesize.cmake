df127271ea98f43e4eff53feeb571ff0498d2b36
Joseph Diza <josephm.diza@gmail.com>
2022-08-03 15:30:49 -0700

    Include lib headers in FetchContent config

--- bytesize.cmake
+++ bytesize.cmake
@@ -3,6 +3,7 @@
 # You can include bytesize into your project in two ways. You can:
 # 1.a Install the library as a package system wide. 
 # 2.b Toggle ${USE_AS_SUBMODULE}, and include library as a git submodule in your project.
+#set(USE_AS_SUBMODULE OFF)
 
 # *Note* that if you are building Bytesize as a subproject. Bytesize depends on log.c and
 # Unity. If these are also configured # as subprojects, then you can include them with:
@@ -11,21 +12,15 @@
 # Given that you are using my cmake-modules here: https://github.com/jmdaemon/cmake-modules
 # Otherwise they must be installed on your system or configured as a subproject manually.
 
-#set(USE_AS_SUBMODULE OFF)
-
 # Library Variables 
 set(LIB_NAME bytesize)
-set(LIB_INCLUDE_NAME "BYTESIZE")
 set(LIB_NAMES bytesize libbytesize)
-
 set(LIB_PUBLIC_HEADER bytesize.h)
-
-set(LIB_SUBPROJECT "${PROJECT_SOURCE_DIR}/subprojects/bytesize")
-set(LIB_HEADERS "${PROJECT_SOURCE_DIR}/subprojects/bytesize/include")
-
 set(LIB_GIT_REPO https://github.com/jmdaemon/bytesize)
 
-# Include these headers in your projects
+## Subproject variables
+set(LIB_SUBPROJECT "${PROJECT_SOURCE_DIR}/subprojects/bytesize")
+set(LIB_SUBPROJECT_INCLUDE "${PROJECT_SOURCE_DIR}/subprojects/bytesize/include")
 
 # If the library hasn't been included
 if (NOT TARGET ${LIB_NAME})
@@ -34,6 +29,9 @@ if (NOT TARGET ${LIB_NAME})
     set(USR /usr/lib)
     set(USR_LOCAL /usr/local/lib)
     set(USR_INCLUDE /usr/include)
+
+    # Contains the directory to the public library's header
+    string(TOUPPER ${LIB_NAME} LIB_INCLUDE_NAME)
 
     set(LIB_USR ${USR}/lib${LIB_NAME}.so)
     set(LIB_LOCAL ${USR_LOCAL}/lib${LIB_NAME}.so)
@@ -46,7 +44,7 @@ if (NOT TARGET ${LIB_NAME})
         add_library(${LIB_NAME} SHARED IMPORTED GLOBAL)
         set_target_properties(${LIB_NAME} PROPERTIES
             IMPORTED_LOCATION ${LIB_USR}
-            PUBLIC_HEADER ${LIB_INCLUDE})
+            PUBLIC_HEADER ${LIB_INCLUDE}) # Include public interface headers
 
         # Set headers variable used for other projects
         set(HEADERS_${LIB_INCLUDE_NAME} ${LIB_INCLUDE})
@@ -58,29 +56,30 @@ if (NOT TARGET ${LIB_NAME})
         add_library(${LIB_NAME} SHARED IMPORTED GLOBAL)
         set_target_properties(${LIB_NAME} PROPERTIES
             IMPORTED_LOCATION ${LIB_LOCAL}
-            PUBLIC_HEADER ${LIB_INCLUDE}) # Include public interface headers
+            PUBLIC_HEADER ${LIB_INCLUDE})
 
-        # Set headers variable used for other projects
         set(HEADERS_${LIB_INCLUDE_NAME} ${LIB_INCLUDE})
     endif()
 else()
     message(STATUS "Configuring ${LIB_NAME} as a subproject")
     if (EXISTS ${LIB_SUBPROJECT})
+        set(SUBPROJECT_INCLUDE ${LIB_SUBPROJECT_INCLUDE}/${LIB_PUBLIC_HEADER})
         if (NOT ${USE_AS_SUBMODULE})
             message(STATUS, "Configuring ${LIB_NAME} with FetchContent")
             # Configure with FetchContent
-            FetchContent_Declare(bytesize
+            FetchContent_Declare(${LIB_NAME}
                 GIT_REPOSITORY  ${LIB_GIT_REPO}
                 SOURCE_DIR      ${LIB_SUBPROJECT})
 
             add_library(${LIB_NAME})
             target_link_libraries(${LIB_NAME} PUBLIC ${LIB_NAME})
-            # Include headers here
+            # Include subproject headers
+            target_include_directories(${LIB_NAME} PUBLIC ${SUBPROJECT_INCLUDE})
+            set_target_properties(${LIB_NAME} PROPERTIES PUBLIC_HEADER ${SUBPROJECT_INCLUDE})
         else()
             message(STATUS, "Configuring ${LIB_NAME} as Git Submodule")
             # Configure as local git submodule / subproject
-            # Note that this requires you to also have the other libraries that
-            # ${LIB_NAME} requires
+            # This builds the library from source (you'll need the library's required build deps)
             add_subdirectory(${LIB_SUBPROJECT})
         endif()
     endif()
