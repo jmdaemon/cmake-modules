56c059eb88266081e4f3dc3a03883f8dbf0f9c2c
Joseph Diza <josephm.diza@gmail.com>
2022-08-03 15:49:46 -0700

    Remove duplicate code in cnc recipe with include_lib

--- cnc.cmake
+++ cnc.cmake
@@ -1,27 +1,21 @@
+# CNC
+# Required Import
+include(Library)
 
-# If the library is installed on our system
-find_library(cnc HINTS /usr/local/lib)
+# Library Variables 
+set(LIB_NAME cnc)
+set(LIB_NAMES cnc libcnc)
+set(LIB_PUBLIC_HEADER cnc.h)
+set(LIB_GIT_REPO https://github.com/jmdaemon/cnc)
 
-# If the library is not installed on our system
-if (NOT TARGET cnc) 
-    # Include the library
-    set(CNC_LIB_SRC /usr/local/lib)
-    set(CNC_LIB_FILE libcnc.so)
-    # Import only if the library was installed system wide
-    if (EXISTS "${CNC_LIB_SRC}/${CNC_LIB_FILE}")
-        import_library(
-            NAME cnc
-            TYPE SHARED
-            FILE ${CNC_LIB_FILE}
-            SOURCE_DIR ${CNC_LIB_SRC})
-    # If the library was not installed system wide
-    else()
-        # Fetch and build it with FetchContent 
-        include(FetchContent)
-        FetchContent_Declare(
-            cnc
-            GIT_REPOSITORY https://github.com/jmdaemon/cnc.git
-            GIT_TAG master)
-        FetchContent_MakeAvailable(cnc)
-    endif()
-endif()
+## Subproject variables
+set(LIB_SUBPROJECT "${PROJECT_SOURCE_DIR}/subprojects/cnc")
+set(LIB_SUBPROJECT_INCLUDE "${PROJECT_SOURCE_DIR}/subprojects/cnc/include")
+
+include_lib(
+        ${LIB_NAME}
+        ${LIB_NAMES}
+        ${LIB_PUBLIC_HEADER}
+        ${LIB_GIT_REPO}
+        ${LIB_SUBPROJECT}
+        ${LIB_SUBPROJECT_INCLUDE})
