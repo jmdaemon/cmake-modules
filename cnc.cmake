d81b491c99c647974dccec93037821b3d624daf6
Joseph Diza <josephm.diza@gmail.com>
2022-08-03 16:05:47 -0700

    Fix missing headers for cnc

--- cnc.cmake
+++ cnc.cmake
@@ -5,12 +5,14 @@ include(Library)
 # Library Variables 
 set(LIB_NAME cnc)
 set(LIB_NAMES cnc libcnc)
-set(LIB_PUBLIC_HEADER cnc.h)
+set(LIB_PUBLIC_HEADER InputParser.h)
 set(LIB_GIT_REPO https://github.com/jmdaemon/cnc)
+
+set(USE_AS_SUBMODULE ON)
 
 ## Subproject variables
 set(LIB_SUBPROJECT "${PROJECT_SOURCE_DIR}/subprojects/cnc")
-set(LIB_SUBPROJECT_INCLUDE "${PROJECT_SOURCE_DIR}/subprojects/cnc/include")
+set(LIB_SUBPROJECT_INCLUDE "${PROJECT_SOURCE_DIR}/subprojects/cnc/include/command")
 
 include_lib(
         ${LIB_NAME}
