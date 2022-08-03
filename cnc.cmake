44d643dc756f8424a708c6ea32e5a237c58a8109
Joseph Diza <josephm.diza@gmail.com>
2022-08-03 16:31:09 -0700

    Use new LIB_SUBPROJECT_DIR var in cnc

--- cnc.cmake
+++ cnc.cmake
@@ -8,11 +8,12 @@ set(LIB_NAMES cnc libcnc)
 set(LIB_PUBLIC_HEADER InputParser.h)
 set(LIB_GIT_REPO https://github.com/jmdaemon/cnc)
 
-set(USE_AS_SUBMODULE ON)
+set(USE_AS_SUBMODULE OFF)
 
 ## Subproject variables
 set(LIB_SUBPROJECT "${PROJECT_SOURCE_DIR}/subprojects/cnc")
-set(LIB_SUBPROJECT_INCLUDE "${PROJECT_SOURCE_DIR}/subprojects/cnc/include/command")
+set(LIB_SUBPROJECT_INCLUDE "${PROJECT_SOURCE_DIR}/subprojects/cnc/include")
+set(LIB_SUBPROJECT_DIR "command")
 
 include_lib(
         ${LIB_NAME}
@@ -20,4 +21,6 @@ include_lib(
         ${LIB_PUBLIC_HEADER}
         ${LIB_GIT_REPO}
         ${LIB_SUBPROJECT}
-        ${LIB_SUBPROJECT_INCLUDE})
+        ${LIB_SUBPROJECT_INCLUDE}
+        ${LIB_SUBPROJECT_DIR}
+        )
