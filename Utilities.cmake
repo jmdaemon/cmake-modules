2b7a13e0d9a8ee5bca907c4ab746cb01c59670ca
Joseph Diza <josephm.diza@gmail.com>
2022-08-03 18:50:21 -0700

    Add Colors to Utilities

--- Utilities.cmake
+++ Utilities.cmake
@@ -1,3 +1,26 @@
+# Colors 
+# Use these for colored status messages
+# Example: message("${Red}This is Red${ColourReset}")
+if(NOT WIN32)
+  string(ASCII 27 Esc)
+  set(ColourReset "${Esc}[m")
+  set(ColourBold  "${Esc}[1m")
+  set(Red         "${Esc}[31m")
+  set(Green       "${Esc}[32m")
+  set(Yellow      "${Esc}[33m")
+  set(Blue        "${Esc}[34m")
+  set(Magenta     "${Esc}[35m")
+  set(Cyan        "${Esc}[36m")
+  set(White       "${Esc}[37m")
+  set(BoldRed     "${Esc}[1;31m")
+  set(BoldGreen   "${Esc}[1;32m")
+  set(BoldYellow  "${Esc}[1;33m")
+  set(BoldBlue    "${Esc}[1;34m")
+  set(BoldMagenta "${Esc}[1;35m")
+  set(BoldCyan    "${Esc}[1;36m")
+  set(BoldWhite   "${Esc}[1;37m")
+endif()
+
 function(lstarget)
     # Set make_library arguments
     set(ARG_PREFIX TARGET)
