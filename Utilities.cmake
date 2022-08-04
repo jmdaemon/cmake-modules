9b16f00dde7e58c2b68ec9629ac7964814b6a19a
Joseph Diza <josephm.diza@gmail.com>
2022-08-03 18:35:16 -0700

    Add logging statements for CMake in Utilities

--- Utilities.cmake
+++ Utilities.cmake
@@ -11,3 +11,42 @@ function(lstarget)
     message(STATUS, "Headers Directory: ${TARGET_HEADERS}")
 endfunction()
 
+# Debugging 
+
+## Logging functions
+set_property(GLOBAL PROPERTY ENABLE_LOGGING OFF)
+
+macro(log_info msg)
+    get_property(IS_ENABLED GLOBAL PROPERTY ENABLE_LOGGING)
+    if (IS_ENABLED)
+        message(STATUS "INFO ${msg}")
+    endif()
+endmacro()
+
+macro(log_debug msg)
+    get_property(IS_ENABLED GLOBAL PROPERTY ENABLE_LOGGING)
+    if (IS_ENABLED)
+        message(STATUS "DEBUG ${msg}")
+    endif()
+endmacro()
+
+macro(log_warn msg)
+    get_property(IS_ENABLED GLOBAL PROPERTY ENABLE_LOGGING)
+    if (IS_ENABLED)
+        message(STATUS "WARN ${msg}")
+    endif()
+endmacro()
+
+macro(log_warn msg)
+    get_property(IS_ENABLED GLOBAL PROPERTY ENABLE_LOGGING)
+    if (IS_ENABLED)
+        message(STATUS "TRACE ${msg}")
+    endif()
+endmacro()
+
+macro(debug variableName)
+    get_property(IS_ENABLED GLOBAL PROPERTY ENABLE_LOGGING)
+    if (IS_ENABLED)
+        debug("${variableName}=\${${variableName}}")
+    endif()
+endmacro()
