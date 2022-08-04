c499d9c9c721ff4517164049831a10ffe3c761a5
Joseph Diza <josephm.diza@gmail.com>
2022-08-03 19:05:26 -0700

    Strip quotes from cnc

--- cnc.cmake
+++ cnc.cmake
@@ -2,18 +2,18 @@
 # Required Import
 include(Library)
 
+set(USE_AS_SUBMODULE OFF)
+
 # Library Variables 
 set(LIB_NAME cnc)
 set(LIB_NAMES cnc libcnc)
 set(LIB_PUBLIC_HEADER InputParser.h)
 set(LIB_GIT_REPO https://github.com/jmdaemon/cnc)
 
-set(USE_AS_SUBMODULE OFF)
-
 ## Subproject variables
-set(LIB_SUBPROJECT "${PROJECT_SOURCE_DIR}/subprojects/cnc")
-set(LIB_SUBPROJECT_INCLUDE "${PROJECT_SOURCE_DIR}/subprojects/cnc/include")
-set(LIB_SUBPROJECT_DIR "command")
+set(LIB_SUBPROJECT ${PROJECT_SOURCE_DIR}/subprojects/cnc)
+set(LIB_SUBPROJECT_INCLUDE ${PROJECT_SOURCE_DIR}/subprojects/cnc/include)
+set(LIB_SUBPROJECT_DIR command)
 
 include_lib(
         ${LIB_NAME}
