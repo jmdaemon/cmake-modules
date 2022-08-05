5ce52f90a7aeb961b0e120289c4fb72babe61fb7
Joseph Diza <josephm.diza@gmail.com>
2022-08-04 21:11:18 -0700

    Use include_lib in Unity cmake module

--- Unity.cmake
+++ Unity.cmake
@@ -1,8 +1,24 @@
-# Build ThrowTheSwitch's unity cmake project
-set(UNITY_NAME Unity)
-add_library(
-    ${UNITY_NAME}
-    STATIC
-    "${PROJECT_SOURCE_DIR}/subprojects/unity/src/unity.c")
+# Unity Testing Framework
+include(Library)
 
-target_include_directories(${UNITY_NAME} PUBLIC ${HEADERS_UNITY})
+set(LIB_NAME Unity)
+set(LIB_NAMES unity libunity)
+set(LIB_PUBLIC_HEADER unity.h)
+set(LIB_HEADER_DIR unity)
+set(LIB_GIT_REPO https://github.com/throwtheswitch/unity)
+
+# Subproject variables
+set(LIB_SUBPROJECT "${PROJECT_SOURCE_DIR}/subprojects/unity")
+set(LIB_SUBPROJECT_INCLUDE "${PROJECT_SOURCE_DIR}/subprojects/unity/src")
+set(LIB_SUBPROJECT_DIR "")
+
+include_lib(
+    NAME ${LIB_NAME}
+    NAMES ${LIB_NAMES}
+    PUB ${LIB_PUBLIC_HEADER}
+    REPO ${LIB_GIT_REPO}
+    TYPE STATIC
+    HDRD ${LIB_HEADER_DIR}
+    SP ${LIB_SUBPROJECT}
+    SPI ${LIB_SUBPROJECT_INCLUDE}
+    SPD ${LIB_SUBPROJECT_DIR})
