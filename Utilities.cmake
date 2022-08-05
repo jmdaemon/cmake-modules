95078724001f94e52a5787fed3d0544473b500ad
Joseph Diza <josephm.diza@gmail.com>
2022-08-05 04:48:03 -0700

    Remove dead code

--- Utilities.cmake
+++ Utilities.cmake
@@ -37,67 +37,40 @@ endfunction()
 # Debugging 
 
 ## Logging functions
-# Use the following to enable viewing logs
-# set(GLOBAL ENABLE_LOGGING ON CACHE INTERNAL "View the logging statements/debug info")
+# Usage:
+# You can either toggle logging on/off:
+#   set(GLOBAL ENABLE_LOGGING ON CACHE INTERNAL "View the logging statements/debug info")
+# Or you can enable specific log levels only:
+#   set(LOG_LEVEL 4 CACHE INTERNAL "Enable only specific log levels")
 
-    #=======
-# Log Levels:
-# 0: None
-# 1: All
-set(LOG_TRACE 2)
-set(LOG_DEBUG 3)
-set(LOG_INFO 4)
-set(LOG_WARN 5)
-set(LOG_ERROR 6)
-#set(LOG_NONE 0)
-#set(LOG_NONE 0)
+# Log levels
+set(LOG_NONE 0)
+set(LOG_TRACE 1)
+set(LOG_DEBUG 2)
+set(LOG_INFO 3)
+set(LOG_WARN 4)
+set(LOG_ERROR 5)
 
-# Use case
-# Enable all logging
-# Enable specific logging levels
-
-#set(LOGLEVEL 0)
-
-#function(log_msg msg loglevel)
 function(log_msg msg upperbound)
-    #if (${loglevel} == )
     if(("${LOG_LEVEL}" STREQUAL "${upperbound}") OR (NOT "${ENABLE_LOGGING}" STREQUAL OFF) OR ("${LOG_LEVEL}" GREATER "${upperbound}"))
         message(STATUS "${msg}")
     endif()
-    #if (ENABLE_LOGGING)
-    #endif()
 endfunction()
 
 function(log_trace msg)
-    #if(("${LOG_LEVEL}" STREQUAL "${LOG_TRACE}") OR ("${ENABLE_LOGGING}") OR ("${LOG_LEVEL}" GREATER "${LOG_TRACE}"))
-    #if(("${LOG_LEVEL}" STREQUAL "${LOG_TRACE}") OR (NOT "${ENABLE_LOGGING}" STREQUAL OFF) OR ("${LOG_LEVEL}" GREATER "${LOG_TRACE}"))
-    #if(("${LOG_LEVEL}" STREQUAL "${LOG_TRACE}") OR ("${LOG_LEVEL}" GREATER "${LOG_TRACE}"))
     log_msg("${Magenta}TRACE${ColorReset} ${msg}" ${LOG_TRACE})
-    #endif()
 endfunction()
 
 function(log_debug msg)
-    #if(("${LOG_LEVEL}" STREQUAL "${LOG_DEBUG}") OR ("${ENABLE_LOGGING}") OR ("${LOG_LEVEL}" GREATER "${LOG_DEBUG}"))
-    #if(("${LOG_LEVEL}" STREQUAL "${LOG_DEBUG}") OR (NOT "${ENABLE_LOGGING}" STREQUAL OFF) OR ("${LOG_LEVEL}" GREATER "${LOG_DEBUG}"))
-    #if(("${LOG_LEVEL}" STREQUAL "${LOG_DEBUG}") OR ("${LOG_LEVEL}" GREATER "${LOG_DEBUG}"))
     log_msg("${Blue}DEBUG${ColorReset} ${msg}" ${LOG_DEBUG})
-    #endif()
 endfunction()
 
 function(log_info msg)
-    #if(("${LOG_LEVEL}" STREQUAL "${LOG_INFO}") OR ("${ENABLE_LOGGING}") OR ("${LOG_LEVEL}" GREATER "${LOG_INFO}"))
-    #if(("${LOG_LEVEL}" STREQUAL "${LOG_INFO}") OR (NOT "${ENABLE_LOGGING}" STREQUAL OFF) OR ("${LOG_LEVEL}" GREATER "${LOG_INFO}"))
-    #if(("${LOG_LEVEL}" STREQUAL "${LOG_INFO}") OR ("${LOG_LEVEL}" GREATER "${LOG_INFO}"))
     log_msg("${Green}INFO${ColorReset} ${msg}" ${LOG_INFO})
-    #endif()
 endfunction()
 
 function(log_warn msg)
-    #if(("${LOG_LEVEL}" STREQUAL "${LOG_WARN}") OR ("${ENABLE_LOGGING}") OR ("${LOG_LEVEL}" GREATER "${LOG_ERROR}"))
-    #if(("${LOG_LEVEL}" STREQUAL "${LOG_WARN}") OR (NOT "${ENABLE_LOGGING}" STREQUAL OFF) OR ("${LOG_LEVEL}" GREATER "${LOG_WARN}"))
-    #if(("${LOG_LEVEL}" STREQUAL "${LOG_WARN}") OR ("${LOG_LEVEL}" GREATER "${LOG_ERROR}"))
     log_msg("${Red}WARN${ColorReset} ${msg}" ${LOG_WARN})
-    #endif()
 endfunction()
 
 function(debug variableName)
