350b3f8455ee60d63efbdb955454a80cb0e6a33e
Joseph Diza <josephm.diza@gmail.com>
2022-08-05 17:27:29 -0700

    Preserve cflags when toggling colored output

--- Color.cmake
+++ Color.cmake
@@ -1,2 +1,2 @@
-set(CMAKE_CXX_FLAGS -fdiagnostics-color=always)
-set(CMAKE_C_FLAGS -fdiagnostics-color=always)
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fdiagnostics-color=always")
+set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -fdiagnostics-color=always")
