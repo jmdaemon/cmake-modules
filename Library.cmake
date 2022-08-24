82bad3bc447431e5bc223194ecaaddd96011ea71
Joseph Diza <josephm.diza@gmail.com>
2022-08-24 16:02:27 -0700

    Prune old Library.cmake functions

--- Library.cmake
+++ Library.cmake
@@ -1,58 +1,15 @@
 # Library.cmake - Include CMake projects and libraries
 
 # Imports
+include(FetchContent)
 include(CMakeParseArguments)    # Parse lists of function arguments
 include(Utilities)              # Includes color, logging statements
 
-# Global constants
+# Constants
 set(USR /usr/lib CACHE INTERNAL "System-wide installed libraries directory")
 set(USR_LOCAL /usr/local/lib CACHE INTERNAL "Locally installed libraries")
 set(USR_INCLUDE /usr/include CACHE INTERNAL "System-wide installed library headers")
 set(USR_LOCAL_INCLUDE /usr/local/include CACHE INTERNAL "Locally installed library headers")
-
-function(make_library)
-    set(ARG_PREFIX LIB)
-    set(_OPTIONS_ARGS )
-    set(_ONE_VALUE_ARGS NAME TYPE CSTANDARD)
-    set(_MULTI_VALUE_ARGS HEADERS SOURCES DEPS SRC_GROUP_FILES)
-    cmake_parse_arguments(${ARG_PREFIX} "${_OPTIONS_ARGS}" "${_ONE_VALUE_ARGS}" "${_MULTI_VALUE_ARGS}" ${ARGN})
-
-    # Show make_library arguments
-    #message(STATUS "======================")
-    #message(STATUS "make_library arguments: ${LIB_NAME}, followed by ${ARGN}")
-    #message(STATUS "LIB_NAME: ${LIB_NAME}")
-    #message(STATUS "LIB_TYPE: ${LIB_TYPE}")
-    #message(STATUS "LIB_CSTANDARD: ${LIB_CSTANDARD}")
-    #message(STATUS "LIB_HEADERS: ${LIB_HEADERS}")
-    #message(STATUS "LIB_SOURCES: ${LIB_SOURCES}")
-    #message(STATUS "LIB_DEPS: ${LIB_DEPS}")
-    #message(STATUS "======================")
-
-    if(${LIB_TYPE} STREQUAL STATIC)
-        # Build a static library
-        set(TARGET ${LIB_NAME}_static)
-    elseif(${LIB_TYPE} STREQUAL SHARED)
-        # Build a shared library
-        set(TARGET ${LIB_NAME})
-    endif()
-    # Add a library target
-    add_library(${TARGET} ${LIB_TYPE})
-
-    # Set the library sources
-    target_sources(${TARGET}
-        PRIVATE ${LIB_SOURCES}
-        PUBLIC  ${LIB_HEADERS})
-
-    # Include the library headers and link the dependencies
-    target_include_directories(${TARGET} PUBLIC ${LIB_HEADERS})
-    target_link_libraries(${TARGET} PRIVATE ${LIB_DEPS})
-    target_compile_features(${TARGET} PUBLIC ${LIB_CSTANDARD})
-
-    source_group(
-        TREE "${PROJECT_SOURCE_DIR}/include"
-        PREFIX "Header Files"
-        FILES ${LIB_SRC_GROUP_FILES})
-endfunction()
 
 # Create either a static or shared library
 function(make_lib)
@@ -101,49 +58,6 @@ function(make_ssl)
             HDR ${LIB_HDR}
             SRCS ${LIB_SRCS}
             DEPS ${LIB_DEPS})
-    endforeach()
-endfunction()
-
-#### make_static_shared_lib
-function(make_static_shared_lib)
-    # Builds both shared, static libraries without specifying ${SRC_GROUP_FILES}
-    set(ARG_PREFIX LIB)
-    set(_OPTIONS_ARGS )
-    set(_ONE_VALUE_ARGS NAME CSTANDARD)
-    set(_MULTI_VALUE_ARGS HEADERS SOURCES DEPS)
-    cmake_parse_arguments(${ARG_PREFIX} "${_OPTIONS_ARGS}" "${_ONE_VALUE_ARGS}" "${_MULTI_VALUE_ARGS}" ${ARGN})
-    set(LIB_TYPES SHARED STATIC)
-
-    foreach(LIB_TYPE ${LIB_TYPES})
-        make_library(
-            NAME "${LIB_NAME}"
-            TYPE ${LIB_TYPE}
-            CSTANDARD "${LIB_CSTANDARD}"
-            HEADERS "${LIB_HEADERS}"
-            SOURCES "${LIB_SOURCES}"
-            DEPS "${LIB_DEPS}")
-    endforeach()
-endfunction()
-
-#### make_static_shared_lib
-function(make_static_shared_lib)
-    # Builds both a shared and a static library target
-    set(ARG_PREFIX LIB)
-    set(_OPTIONS_ARGS )
-    set(_ONE_VALUE_ARGS NAME)
-    set(_MULTI_VALUE_ARGS HEADERS SOURCES DEPS)
-    cmake_parse_arguments(${ARG_PREFIX} "${_OPTIONS_ARGS}" "${_ONE_VALUE_ARGS}" "${_MULTI_VALUE_ARGS}" ${ARGN})
-    set(LIB_TYPES SHARED STATIC)
-
-    foreach(LIB_TYPE ${LIB_TYPES})
-        make_library(
-            NAME "${LIB_NAME}"
-            TYPE ${LIB_TYPE}
-            CSTANDARD "${LIB_CSTANDARD}"
-            HEADERS "${LIB_HEADERS}"
-            SOURCES "${LIB_SOURCES}"
-            DEPS "${LIB_DEPS}"
-            SRC_GROUP_FILES "${LIB_SRC_GROUP_FILES}")
     endforeach()
 endfunction()
 
